fa08b9cce6f4303566768e40db94edb380b966a0
Yazan-al-kass <yazan.al-kass@quizplus.com>
2024-11-17 14:11:06 +0200

    first commit

---
+++ src/components/2-hero/Hero.jsx
@@ -0,0 +1,80 @@
+import Lottie from "lottie-react";
+import "./hero.css";
+import devAnimation from "../../animation/dev.json";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
+const Hero = () => {
+  const lottieRef = useRef();
+
+  return (
+    <section className="hero flex">
+      <div className="left-section  ">
+        <div className="parent-avatar flex">
+          <motion.img
+            initial={{ transform: "scale(0)" }}
+            animate={{ transform: "scale(1.1)" }}
+            transition={{ damping: 6, type: "spring", stiffness: 100 }}
+            src="./me.png"
+            className="avatar"
+            alt=""
+          />
+          <div className="icon-verified"></div>
+        </div>
+
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2 }}
+          className="title"
+        >
+          Software Engineer, Frontend developer React JS.
+        </motion.h1>
+
+        <p className="sub-title">
+          I’m Yazan Abu Alkas, a passionate freelance software engineer and web
+          developer from Gaza City, dedicated to crafting innovative and
+          efficient digital solutions.
+        </p>
+        <div className="all-icons flex">
+          <a
+            href="https://www.instagram.com/yazn_al_kass/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="icon icon-instagram"></div>
+          </a>
+          <a
+            href="https://github.com/Yazan-kass"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="icon icon-github"></div>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/yazan-abu-al-kass/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <div className="icon icon-linkedin"></div>
+          </a>
+        </div>
+      </div>
+
+      <div className="right-section animation ">
+        <Lottie
+          lottieRef={lottieRef}
+          className=""
+          onLoadedImages={() => {
+            // @ts-ignore
+            // https://lottiereact.com/
+            lottieRef.current.setSpeed(0.5);
+          }}
+          animationData={devAnimation}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ src/components/3-main/myProjects.js
@@ -0,0 +1,99 @@
+export const myProjects = [
+  //-------------react projects
+
+  {
+    projectTitle: "gym exercises",
+    category: ["react"],
+    imgPath: "./images/react/1-react.jpeg",
+    gitPath: "https://github.com/Yazan-kass/gym-exercises",
+    vercelpath: "https://gym-exercises-bay.vercel.app/",
+  },
+  {
+    projectTitle: "prayers times app",
+    category: ["react"],
+    imgPath: "./images/react/2-react.png",
+    gitPath: "https://github.com/Yazan-kass/prayers-times-app",
+    vercelpath: "https://prayer-times-app-one.vercel.app/",
+  },
+  {
+    projectTitle: "To Do List app",
+    category: ["react"],
+    imgPath: "./images/react/3-react.png",
+    gitPath: "https://github.com/Yazan-kass/To-Do_List-ReactTrining-",
+    vercelpath: "https://to-do-list-sooty-three.vercel.app/",
+  },
+  {
+    projectTitle: "chat app",
+    category: ["react"],
+    imgPath: "./images/react/4-react.jpg",
+    gitPath: "https://github.com/Yazan-kass/react-firebase-chat",
+    vercelpath: "https://vercel.com/yazan-jamal-abu-alkass-projects",
+  },
+  {
+    projectTitle: "movie app",
+    category: ["react"],
+    imgPath: "./images/react/5-react.jpeg",
+    gitPath: "https://github.com/Yazan-kass/react-firebase-chat",
+    vercelpath: "https://movie-beryl-eight.vercel.app/",
+  },
+  {
+    projectTitle: "task app",
+    category: ["react"],
+    imgPath: "./images/react/6-react.png",
+    gitPath: "https://github.com/Yazan-kass/tasks-app-redux-fb",
+    vercelpath: "https://vercel.com/yazan-jamal-abu-alkass-projects",
+  },
+  {
+    projectTitle: "galary app",
+    category: ["react"],
+    imgPath: "./images/react/7-react.png",
+    gitPath: "https://github.com/Yazan-kass/Gallery-Photo-React-Trining-",
+    vercelpath: "https://gallery-rose-delta.vercel.app/",
+  },
+  //--------------js projects
+  {
+    projectTitle: "galary app",
+    category: ["js"],
+    imgPath: "./images/js/1-js.png",
+    gitPath: "https://github.com/Yazan-kass/Gallery-Photo-React-Trining-",
+    vercelpath:
+      "https://vercel.com/yazan-jamal-abu-alkass-projects/gallery-f1se",
+  },
+  {
+    projectTitle: "shop app",
+    category: ["js"],
+    imgPath: "./images/js/2-js.png",
+    gitPath: "https://github.com/Yazan-kass/my-work/tree/main/store",
+    vercelpath: "https://shop-psi-ruby.vercel.app/",
+  },
+  {
+    projectTitle: "coofe app",
+    category: ["js"],
+    imgPath: "./images/js/3-js.jpg",
+    gitPath: "https://github.com/Yazan-kass",
+    vercelpath: "https://vercel.com/yazan-jamal-abu-alkass-projects",
+  },
+
+  //--------------html projects
+  {
+    projectTitle: "anmation app",
+    category: ["css"],
+    imgPath: "./images/html/1-html.png",
+    gitPath: "https://github.com/Yazan-kass/my-work/tree/main/animated-webset",
+    vercelpath: "https://vercel.com/yazan-jamal-abu-alkass-projects",
+  },
+  {
+    projectTitle: "login app",
+    category: ["css"],
+    imgPath: "./images/html/2-html.png",
+    gitPath: "https://github.com/Yazan-kass/",
+    vercelpath: "https://vercel.com/yazan-jamal-abu-alkass-projects",
+  },
+  {
+    projectTitle: "platform app",
+    category: ["css"],
+    imgPath: "./images/html/3-html.jpg",
+    gitPath: "https://github.com/Yazan-kass/my-work/tree/main/Template2",
+    vercelpath: "https://vercel.com/yazan-jamal-abu-alkass-projects",
+  },
+];
